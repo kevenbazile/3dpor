610e61d9e92245c64ebff902a215e1a6d6c5568b
kevenbazile <ephrim.bazile@gmail.com>
2024-03-08 18:58:58 -0800

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import nfl from './nfl.png'
+import shopify from './shopify.png'
+import whale from './whale.png'
+import mickey from './mickey.png'
+import logo from './logo.svg'
+
+export {
+    hero,
+    nfl,
+    shopify,
+    whale,
+    mickey,
+    logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,229 @@
+import { nfl, shopify, whale, mickey } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Software Developer",
+        company_name: "WhaleFilm",
+        icon: whale,
+        iconBg: "#accbe1",
+        date: "March 2020 - April 2021",
+        points: [
+            "Developing and maintaining web applications using Arduino and other related technologies. To create an immersive Led experience for their event.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Electrical Engineer",
+        company_name: "Walt Disney",
+        icon: mickey,
+        iconBg: "#fbc3bc",
+        date: "Nov 2022 - Feb 2022",
+        points: [
+            "Developing and maintaining hardware and implementing robotics with related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Shopify",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "DevOps Developer",
+        company_name: "NFL",
+        icon: nfl,
+        iconBg: "#a2d2ff",
+        date: "Jan 2022 - 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: 'https://www.upwork.com/freelancers/~013a77d3cb50db8613?mp_source=share',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/kevenbazile',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/ephrim-bazile-8a626b214/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'RapidApi',
+        description: 'Birds Nest Cafe API (RapidAPI) This project is an API wrapper for the Birds Nest Cafe in Los Angeles, using RapidAPI. The API allows users to access information about the restaurant such as menu items, hours of operation, and reviews.',
+        link: 'https://github.com/kevenbazile/rapid-api',
+     
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'CRUD-Application',
+        description: 'Create, Read, Update, Delete) application that allows users to perform these actions on a specific set of data.(PHP backend, React frontend, node.js server and MySQL database and Angular used for handline real time updates',
+        link: 'https://github.com/kevenbazile/CRUD-APPLICATION',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Space Invaders',
+        description: 'pace Invaders: Start by providing a brief overview of the project, including the purpose and main features. Then, list the technologies used and any dependencies that need to be installed. Provide instructions for running the game, including any command line arguments that may be required.',
+        link: 'https://github.com/kevenbazile/Space-invaders-',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Booking System',
+        description: 'A Booking System: This project is a platform that allows users to book appointments with service providers. The back-end is built using PHP',
+        link: 'https://github.com/kevenbazile/Booking-system',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Real-Estate Application',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/adrianhajdin/projects_realestate',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    }
+];
